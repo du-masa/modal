80f065e837185c97a3be57ad7f838d86b15028bc
zap-mueda <m_ueda@zappallas.com>
2018-04-23 15:43:31 +0900

    問題用

--- assets/vue/vue-main.js
+++ assets/vue/vue-main.js
@@ -3,25 +3,7 @@ import '../scss/vue-style.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  // タブ処理
+  // モーダル処理
   (function() {
-    new Vue({
-      el: "#modal",
-      data: function() {
-        return {
-          activeModal: ''
-        }
-      },
-      methods: {
-        openModal(selectedModalName) {
-          this.activeModal = selectedModalName;
-        },
-        closeModal(e) {
-          if (e.currentTarget ===　e.target) {
-            this.activeModal = '';
-          }
-        }
-      }
-    });
   }());
 })
